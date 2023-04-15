168b6fb49bab7823b8d3801196c36630db2f60d1
kTegowski <juliaszalwinska@MacBook-Air-Julia-2.local>
2023-04-15 17:30:04 +0200

    Create new XCode project

---
+++ meetNewCountries/meetNewCountries/meetNewCountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  meetNewCountriesApp.swift
+//  meetNewCountries
+//
+//  Created by Konrad Tęgowski on 15/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct meetNewCountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
